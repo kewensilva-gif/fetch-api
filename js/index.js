f91b2fc33437400b89d155842e67e526a964153f
Kéwen Silva <kewensilva58@gmail.com>
2022-11-04 23:24:40 -0300

    pegando o id na url

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+
+//Get id from URL
+const params = new URLSearchParams(window.location.search);
+const postId = params.get("id");
+
+//Get all posts
+async function getAllPosts() {
+    //Esperando resposta da api
+    const response = await fetch(url);
+
+    //Convertendo os dados da api em objeto json
+    const data = await response.json();
+
+    //Escondendo o carregando
+    loadingElement.classList.add("hide");
+
+    //A função map vai fazer com que se passe por cada um dos elementos obtidos na requisição
+    data.map((post) =>{
+
+        //Criando os elementos html 
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const body = document.createElement("p");
+        const link = document.createElement("a");
+
+        //Modificando o texto do elementos
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "Ler";
+
+        //Inserindo o atributo href no elemento link e passando a url da pagina post.html e o id da api
+        link.setAttribute("href", `/post.html?id=${post.id}`);
+
+        //Inserindo todos os elementos na div
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+
+        //Inserindo a div criada na div posts container
+        postsContainer.appendChild(div);
+    })
+}
+
+if(!postId)
+    getAllPosts();
+else
+    console.log(postId);
